241f0597c995f57f7a7904e5bcc4f73b15f0f141
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2024-02-27 23:09:32 -0300

    atvwhile

---
+++ atvwhile.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int n, contador = 0;
+    scanf("%i", &n);
+    while (contador < n){
+        printf("Eu adoro programar\n");
+        contador = contador +1;
+    }
+return 0;
+}
